48935f41bb5c2172978c8c61f7d4545ac7fadfa6
matheusdiasz2007 <matheus108532@gmail.com>
2026-04-23 20:16:35 -0300

    novas alterações

---
+++ Exercicios/exercicio002.py
@@ -0,0 +1,2 @@
+nome = input("digite seu nome: ")
+print(f"É um prazer em te conhecer, {nome}!")
---
+++ Exercicios/exercicio003.py
@@ -0,0 +1,5 @@
+a = input("digite um valor:")
+b = input("digite outro valor:")
+
+soma = int(a) + int(b)
+print(f"A soma entre {a} e {b} é igual a {soma}.")  
