d89b5c6c0ffccdfc82a4455809fc4073fadd59ed
matheusdiasz2007 <matheus108532@gmail.com>
2026-04-23 21:02:54 -0300

    add

--- Exercicios/exercicio011.py
+++ Exercicios/exercicio011.py
@@ -1,5 +1,7 @@
 x = float(input("Largura da parede: "))
 y = float(input("Altura da parede: "))  
-print(f"A área da parede é de {x * y} metros quadrados.")
+
 tinta = (x * y) / 2
+
+print(f"A área da parede é de {x * y} metros quadrados.")
 print(f"A quantidade de tinta necessária para pintar a parede é de {tinta} litros.")
---
+++ Exercicios/exercicio014.py
@@ -0,0 +1,3 @@
+Celsius = float(input("Digite a temperatura em Celsius: "))
+Fahrenheit = (Celsius * 9/5) + 32
+print(f"A temperatura em Fahrenheit é de {Fahrenheit:.2f}°F.")
